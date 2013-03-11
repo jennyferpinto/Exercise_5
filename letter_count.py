c450f1d8b479573df4d72877802145a12d7f5e03
Jennyfer <pinto.jennyfer@gmail.com>
2013-03-11 10:28:58 -0700

    fixed version

--- letter_count.py
+++ letter_count.py
@@ -5,24 +5,26 @@ script, filename = argv
 print "Type the filename:"
 file_name = raw_input("> ")
 
-open_file = open(file_name)
+open_file = open(file_name).read()
 
-#1 - i have to make all the strings within the list lowercase
+print open_file
 
-def text_editor(the_text):
-    for char in the_text:
-        return char
-        continue
-        str.lower(char)
-        
-            
-edited_text = text_editor(open_file)
+     
+#new_file = open open_file.read())
 
-a_count = 0
-for char in edited_text:
-    edited_text.read(1)   
-    if char == ord('a'):
-        a_count = a_count + 1
-    else:
-        continue
-    print a_count
+# making file lowercase
+lowercase_file = open_file.lower()
+
+for char in new_file
+
+
+#edited_text = text_editor(open_file)
+
+#a_count = 0
+#for char in edited_text:
+ #   edited_text.read(1)   
+  #  if char == ord('a'):
+   #     a_count = a_count + 1
+    #else:
+     #   continue
+    #print a_count
